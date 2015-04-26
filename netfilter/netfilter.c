e205862afed96e11f55916f6808faf812c0ba20a
Ruofan Ding <segfault0x@gmail.com>
2015-04-26 18:32:56 -0400

    add clean_filter to remove expired filter rules

--- netfilter/netfilter.c
+++ netfilter/netfilter.c
@@ -10,12 +10,13 @@
 #include <signal.h> //SIGALRM
 #include <stdio.h>
 #include <string.h> //memcpy memset
-
+#include <time.h>  //time
 #include "netfilter.h"
 
 #define NFQUEUE_NUM 0
-
-void print_addr(char *msg, struct in_addr a){
+#define T_TEMP 1000
+//============================Helper functions=======================
+inline void print_addr(char *msg, struct in_addr a){
   printf("%s %s\n", msg, inet_ntoa(a));
 }
 
@@ -25,6 +26,17 @@ inline int equalAddr(struct in_addr* a1, struct in_addr* a2)
   printf("with %s\n", inet_ntoa(*a2));
   return !memcmp(a1, a2, sizeof(struct in_addr));
 }
+
+inline void print_flow(struct flow* flow){
+  printf("%s->", inet_ntoa(flow->src_addr));
+  int i;
+  for(i = 0; i < flow->number; i++){
+    printf("%s->", inet_ntoa(flow->route_record[i].addr));
+  }
+  printf("%s\n", inet_ntoa(flow->dest_addr));
+}
+//============================Helper functions=======================
+
 
 //============================Start of Intercept=======================
 InterceptRule intercept_rule_array[INTERCEPT_RULE_SIZE];
@@ -87,6 +99,7 @@ void intercept(struct in_addr src_addr, struct in_addr dest_addr, struct iphdr* 
 
 //============================Start of Intercept=======================
 struct flow filter_rule_array[FILTER_RULE_SIZE];
+time_t filter_rule_expire[FILTER_RULE_SIZE];
 int filter_rule_used[FILTER_RULE_SIZE];
 int filter_rule_number = 0;
 pthread_mutex_t filter_lock;
@@ -116,21 +129,49 @@ void free_filter_rule_spot(int index)
   pthread_mutex_unlock(&filter_lock);
 }
 
-void add_filter_temp(struct flow* flow_pt)
+void clean_filter_rule()
+{
+  time_t current;
+  int i, counter;
+  while(1){
+    current = time(NULL);
+    for(i = 0, counter = 0; i < FILTER_RULE_SIZE; i++){
+      if(counter >= filter_rule_number){
+	break;
+      }
+
+      if(filter_rule_used[i]){
+	counter ++;
+	//Remove the filter rule if it is expired
+	if( current >= filter_rule_expire[i] ){
+	  printf("Filter rule %d expire\n", i);
+	  print_flow(&filter_rule_array[i]);
+	  fflush(stdout);
+	  free_filter_rule_spot(i);
+	}
+      }
+    }
+    sleep(1);
+  }
+}
+
+int add_filter_temp(struct flow* flow_pt)
 {
   int filter_index = get_filter_rule_spot();
   if(filter_index == -1){
     printf("Fail to insert filter rule\n");
     fflush(stdout);
-    return;
+    return 0;
   }
 
+  filter_rule_expire[filter_index] = time(NULL) + T_TEMP;
   memcpy(&filter_rule_array[filter_index], flow_pt, sizeof(struct flow));
   /*
   print_addr("add_filter dest", filter_rule_array[filter_index].dest_addr);
   print_addr("add_filter src", filter_rule_array[filter_index].src_addr);
   */
-  return ;
+  printf("Add filter rule %d\n",filter_index);
+  return 1;
 }
 
 int match_filter_rule(struct flow* rule, struct flow* flow){
@@ -205,8 +246,8 @@ int cb (struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
   dest_addr.s_addr = ip_info->daddr;
   src_addr.s_addr = ip_info->saddr;
 
-  //printf("From %s", inet_ntoa(src_addr));
-  //printf("to %s\n", inet_ntoa(dest_addr));
+  printf("From %s", inet_ntoa(src_addr));
+  printf("to %s\n", inet_ntoa(dest_addr));
   intercept(src_addr, dest_addr, ip_info);
 
   struct flow flow;
@@ -215,24 +256,28 @@ int cb (struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
   flow.number = 0;
 
   if(filter_out(&flow)){
-    //printf("packet dropped\n");
+    printf("packet dropped\n");
     verdict = nfq_set_verdict (qh, id, NF_DROP, 0, NULL);
   }else{
-    //printf("packet passed\n");
+    printf("packet passed\n");
     verdict = nfq_set_verdict (qh, id, NF_ACCEPT, ret, buffer);
   }
-  //printf("\n");
+  printf("\n");
   fflush(stdout);
   return verdict;
 }
 
-void set_up_nfq()
+struct nfq_handle * set_up_nfq()
 {
-  //memset(intercept_rule_used, 0, sizeof(intercept_rule_used));
-  //pthread_mutex_init(&intercept_lock, NULL);
+  memset(intercept_rule_used, 0, sizeof(intercept_rule_used));
+  pthread_mutex_init(&intercept_lock, NULL);
 
   memset(filter_rule_used, 0, sizeof(filter_rule_used));
   pthread_mutex_init(&filter_lock, NULL);
+
+  //Run filter clean thread
+  pthread_t pid;
+  pthread_create(&pid, NULL, clean_filter_rule, NULL);
 
   struct nfq_handle * h = nfq_open();
   if (!h) {
@@ -265,13 +310,16 @@ void set_up_nfq()
     fprintf(stderr, "can't set packet_copy mode\n");
     exit(1);
   }
+  return h;
+}
 
+void run_nfq(struct nfq_handle *h){
   int fd = nfq_fd(h);
   int rv;
   char buf[0xffff];
+
   while ((rv = recv(fd, (void*)buf, sizeof(buf), 0)) >= 0) {
     printf("pkt received\n");
     nfq_handle_packet(h, buf, rv);
   }
 }
-
